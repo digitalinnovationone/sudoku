52bedcd6054fdc40211e4edd35551f0719bf76d6
vanmtv <rjs.vanessa@gmail.com>
2026-02-19 18:02:46 -0300

    Create Constants class and Sudoku Controller

---
+++ src/br/com/dio/controller/SudokuController.java
@@ -0,0 +1,166 @@
+package br.com.dio.controller;
+
+import br.com.dio.model.Board;
+
+import br.com.dio.model.Space;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import static br.com.dio.Main.*;
+import static br.com.dio.util.BoardTemplate.*;
+import static br.com.dio.util.Constants.*;
+import static java.util.Objects.*;
+
+/**
+ * Gerencia o ciclo de vida do jogo Sudoku
+ * @author Vanessa Regina
+ */
+public class SudokuController {
+    private SudokuController() {
+        /* This utility class should not be instantiated */
+    }
+
+    private static Board board;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void startGame(final Map<String, String> positions) {
+        if (nonNull(board)){
+            System.out.println(JOGO_JA_INICIADO);
+            return;
+        }
+
+        List<List<Space>> spaces = new ArrayList<>();
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            spaces.add(new ArrayList<>());
+            for (int j = 0; j < BOARD_LIMIT; j++) {
+                String positionKey = "%s,%s".formatted(i, j);
+                var positionConfig = positions.get(positionKey);
+
+                int expected = 0;
+                boolean fixed = false;
+
+                if (positionConfig != null) {
+                    String[] configParts = positionConfig.split(",");
+                    expected = Integer.parseInt(configParts[0]);
+                    fixed = Boolean.parseBoolean(configParts[1]);
+                }
+                var currentSpace = new Space(expected, fixed);
+                spaces.get(i).add(currentSpace);
+            }
+        }
+
+        board = new Board(spaces);
+        System.out.println(JOGO_PRONTO_COMECAR);
+    }
+
+
+    public static void inputNumber() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        System.out.println(INPUT_COLUNA);
+        var col = runUntilGetValidNumber(0, 8);
+        System.out.println(INPUT_LINHA);
+        var row = runUntilGetValidNumber(0, 8);
+        System.out.printf(INPUT_NUMERO, col, row);
+        var value = runUntilGetValidNumber(1, 9);
+        if (!board.changeValue(col, row, value)){
+            System.out.printf(POSITION, col, row);
+        }
+    }
+
+    public static void removeNumber() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        System.out.println(INPUT_COLUNA);
+        var col = runUntilGetValidNumber(0, 8);
+        System.out.println(INPUT_LINHA);
+        var row = runUntilGetValidNumber(0, 8);
+        if (!board.clearValue(col, row)){
+            System.out.printf(POSITION, col, row);
+        }
+    }
+
+    public static void showCurrentGame() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        var args = new Object[81];
+        var argPos = 0;
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            for (var col: board.getSpaces()){
+                args[argPos ++] = " " + ((isNull(col.get(i).getActual())) ? " " : col.get(i).getActual());
+            }
+        }
+        System.out.println(BOARD_STATUS);
+        System.out.printf((BOARD_TEMPLATE) + "\n", args);
+    }
+
+    public static void showGameStatus() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        System.out.printf(JOGO_STATUS_ATUAL, board.getStatus().getLabel());
+        if(board.hasErrors()){
+            System.out.println(JOGO_COM_ERROS);
+        } else {
+            System.out.println(JOGO_SEM_ERROS);
+        }
+    }
+
+    public static void clearGame() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        System.out.println(CERTEZA_LIMPAR);
+        var confirm = scanner.next();
+        while (!confirm.equalsIgnoreCase(SIM) && !confirm.equalsIgnoreCase(NAO)){
+            System.out.println(INFORME_SIM_NAO);
+            confirm = scanner.next();
+        }
+
+        if(confirm.equalsIgnoreCase(SIM)){
+            board.reset();
+        }
+    }
+
+    public static void finishGame() {
+        if (isNull(board)){
+            System.out.println(JOGO_NAO_INICIADO);
+            return;
+        }
+
+        if (board.gameIsFinished()){
+            System.out.println(PARABENS);
+            showCurrentGame();
+            board = null;
+        } else if (board.hasErrors()) {
+            System.out.println(VERIFIQUE_SEU_BOARD_E_AJUSTE);
+        } else {
+            System.out.println(PREENCHER_ESPACO);
+        }
+    }
+
+    public static int runUntilGetValidNumber(final int min, final int max){
+        var current = scanner.nextInt();
+        while (current < min || current > max){
+            System.out.printf(INPUT_NUM_ENTRE_MIN_MAX, min, max);
+            current = scanner.nextInt();
+        }
+        return current;
+    }
+}
